15cb194e34ea5b9f5826770cd3d9801b0e046ba4
kamiff lee <kamiff@qq.com>
2020-06-17 08:31:05 +0800

    change namespace

--- src/Spring/Spring.Core/Context/Support/NetCore/ContextCreaterExtensions.cs
+++ src/Spring/Spring.Core/Context/Support/NetCore/ContextCreaterExtensions.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Spring.Context;
+using Spring.Context.Support;
 using Spring.Objects.Factory;
 using Spring.Util;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Spring.Context.Support
+namespace Spring.Extensions.DependencyInjection
 {
     /// <summary>
-    /// 
+    /// Extension methods for setting up Spring IOC Container in an <see cref="IServiceCollection" />.
     /// </summary>
+    /// <author>kamiff lee</author>
     public static class ContextCreaterExtensions
     {
         /// <summary>
@@ -19,7 +22,7 @@ namespace Spring.Context.Support
         /// </summary>
         private const string NET_CORE_CONFIG_OBJECT_NAME = "NetCoreConfig";
         /// <summary>
-        /// Init Spring Ioc Context and set into services
+        /// Init Spring Ioc Context and set into services <see cref="IServiceCollection"/>
         /// </summary>
         /// <param name="services"></param>
         /// <param name="configuration"></param>
@@ -35,14 +38,14 @@ namespace Spring.Context.Support
                 {
                     netCoreConfigObjectName = NET_CORE_CONFIG_OBJECT_NAME;
                 }
-
-                if (springContext.ContainsObject(netCoreConfigObjectName))
+                IConfigurableApplicationContext ctx = (IConfigurableApplicationContext)springContext;
+                if (ctx.ObjectFactory.ContainsSingleton(netCoreConfigObjectName))
                 {
                     throw new ObjectDefinitionStoreException(string.Format("Object name:{0} is already in use", netCoreConfigObjectName));
                 }
                 else
                 {
-                    ((IConfigurableApplicationContext)springContext).ObjectFactory.RegisterSingleton(netCoreConfigObjectName, configuration);
+                    ctx.ObjectFactory.RegisterSingleton(netCoreConfigObjectName, configuration);
                 }
             }
             services.AddSingleton(springContext);
